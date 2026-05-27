dfb3658535ddfb45985f31af2e87c88e50450fa5
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-05-27 12:23:00 +0545

    Update DAY1.sql

--- DAY1.sql
+++ DAY1.sql
@@ -35,4 +35,49 @@ FROM play_history
 GROUP BY user_name, hour_of_day
 ORDER BY user_name,hour_of_day;
 
---5.
+--5.Write a query to find the average rating for each genre, but only 
+--include genres that have at least 3 songs. Show genre and
+--avg_rating (rounded to 2 decimal places). 
+--Order by highest average rating first.
+SELECT genre,ROUND(AVG(rating),2) as avg_rating
+FROM songs
+GROUP BY genre
+HAVING COUNT(song_id)>=3
+ORDER BY avg_rating DESC;
+
+--6. Write a query to find the highest rated song for each artist. 
+--Show artist, title, and rating. Each artist should appear only once. 
+--Order by artist name.
+SELECT DISTINCT ON (artist) artist,title,rating
+FROM songs
+ORDER BY artist,rating DESC;
+
+--7.Write a query to find users who have listened to the same song multiple
+--times (same user, same song, more than once). 
+--Show user_name, song_id, and play_count. 
+--Order by play_count descending.
+SELECT user_name,song_id,COUNT(history_id) AS play_count
+FROM play_history
+GROUP BY user_name,song_id
+HAVING COUNT(history_id)>1
+ORDER BY play_count DESC;
+
+--8.Write a query to find all Pop songs with rating greater than 4.5. 
+--Show title, artist, rating and rank. Order by rating from highest to lowest.
+SELECT title,artist,rating ,ROW_NUMBER()OVER(ORDER BY rating DESC) AS rank
+FROM songs 
+WHERE genre='Pop' AND rating > 4.5
+ORDER BY rating DESC;
+
+--9.Write a query to find users who have more than 50 total plays. 
+--Show user_name and total_plays. Order by highest plays first.
+SELECT user_name,COUNT(history_id) AS total_plays
+FROM play_history 
+GROUP BY user_name
+HAVING COUNT(history_id)>50
+ORDER BY total_plays DESC;
+
+--10.
+
+
+
