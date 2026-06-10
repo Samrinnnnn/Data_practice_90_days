5c0c03b0e7a0b1b714f874731053abf38ab21773
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-06-10 17:17:08 +0545

    Update DAY2.sql

--- DAY2.sql
+++ DAY2.sql
@@ -1,1 +1,17 @@
+--FROM https://www.sqlnoir.com/ CASES 
+/* Case #001: The Vanishing Briefcase
+Set in the gritty 1980s, a valuable briefcase has disappeared from the Blue Note Lounge.
+A witness reported that a man in a trench coat was seen fleeing the scene. 
+Investigate the crime scene, review the list of suspects, and examine interview transcripts to reveal the culprit. */
+SELECT name from suspects where id=183;
+
+/* Case #002: The Stolen Sound
+In the neon glow of 1980s Los Angeles, the West Hollywood Records store was rocked by a daring theft.
+A prized vinyl record, worth over $10,000, vanished during a busy evening, leaving the store owner desperate for answers. 
+Vaguely recalling the details, you know the incident occurred on July 15, 1983, at this famous store.
+Your task is to track down the thief and bring them to justice. */
+SELECT *FROM suspects where id=97;
+
+
+
 
