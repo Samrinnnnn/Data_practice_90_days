2dfb5e1b21ccf8a51d567dbfaad82931003eaf5e
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-06-17 20:13:42 +0545

    Update DAY3.sql

--- DAY3.sql
+++ DAY3.sql
@@ -1,1 +1,7 @@
+# DBQuacks Christmas Heist Solutions
 
+---My solutions to the [DBQuacks Christmas Heist 2025](https://dbquacks.com/challenges?series=christmas) SQL challenges (powered by DuckDB).
+
+--Challenge 1: The Missing Presents Report
+--Return the total number of missing presents.
+SELECT SUM(quantity_missing) as missing_presents FROM missing_presents_report;
