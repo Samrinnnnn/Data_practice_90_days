a6c73b16fc12ffab044083d07b9e4f703673760e
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-06-17 20:28:53 +0545

    Update DAY3.sql

--- DAY3.sql
+++ DAY3.sql
@@ -5,3 +5,10 @@
 --Challenge 1: The Missing Presents Report
 --Return the total number of missing presents.
 SELECT SUM(quantity_missing) as missing_presents FROM missing_presents_report;
+
+--Challenge 2: The Suspect Pool
+--Count how many volunteers have an experience_level of 7 or higher. You will be working with the `volunteer_ducks` table
+SELECT COUNT(experience_level )
+  FROM volunteer_ducks 
+  GROUP BY experience_level
+    HAVING COUNT(experience_level) >=7;
